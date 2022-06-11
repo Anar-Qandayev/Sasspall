98e820982c7487425bc709579e317cd5635c6cac
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-06-11 11:48:38 +0400

    commit

---
+++ Sasspal/Sasspal/Models/Slider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sasspal.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Desc { get; set; }
+    }
+}
---
+++ Sasspal/Sasspal/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using Sasspal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sasspal.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+    }
+}
